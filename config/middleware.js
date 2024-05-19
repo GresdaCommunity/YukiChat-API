63b8ec43851eafb0f41d7fab487b6c8899bb59a0
kvnrynfl <kevinreynaufal2004@gmail.com>
2024-05-19 07:43:57 +0700

    feat: implements body-parser middleware

--- config/middleware.js
+++ config/middleware.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.json());
 app.use(cors());
 app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 module.exports = app;
