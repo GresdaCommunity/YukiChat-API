c299819cab6e4a33833a7707f3b999834644698a
kvnrynfl <kevinreynaufal2004@gmail.com>
2024-05-19 07:41:28 +0700

    refactor: changed the way env is called

--- utils/jwtUtils.js
+++ utils/jwtUtils.js
@@ -1,18 +1,11 @@
 const jwt = require('jsonwebtoken');
 
-const jwtSecret = process.env.JWT_SECRET;
-
 const generateToken = (payload) => {
-    return jwt.sign(payload, jwtSecret, { expiresIn: '1h' });
+    return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
 };
 
 const verifyToken = (token) => {
-    try {
-        const decoded = jwt.verify(token, jwtSecret);
-        return decoded;
-    } catch (err) {
-        throw new Error('Token tidak valid atau kedaluwarsa');
-    }
+    return jwt.verify(token, process.env.JWT_SECRET);
 };
 
 module.exports = { generateToken, verifyToken };
